b0d29357b9b716ca3b6b7c09b868593653148d96
Pabloino <pablo.inoriza@mercadolibre.com>
2021-11-30 12:55:30 -0300

    Agrego requests para los permisos

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.myapplication
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,11 +28,28 @@ class MainActivity : AppCompatActivity() {
         val callButton = findViewById<Button>(R.id.button)
 
         callButton.setOnClickListener {
-            val callIntent = Intent(Intent.ACTION_DIAL);
-            val number = "1122334455"
-            callIntent.data = Uri.parse("tel:$number")
-            startActivity(callIntent)
+            requestPermissions()
+            if (checkCallLogPermissions()) {
+                val callIntent = Intent(Intent.ACTION_DIAL)
+                val number = "1122334455"
+                callIntent.data = Uri.parse("tel:$number")
+                startActivity(callIntent)
+            }
         }
+    }
+
+    private fun checkPermission(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun checkCallLogPermissions(): Boolean {
+        return checkPermission(Manifest.permission.WRITE_CALL_LOG)
+                && checkPermission(Manifest.permission.READ_CALL_LOG)
+                && checkPermission(Manifest.permission.READ_PHONE_STATE)
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_CALL_LOG, Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_PHONE_STATE), 0)
     }
 
 }
